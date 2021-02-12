9908b303ed1cd63bba4e720dbc67c9a590bcbe37
Nency Shobhashana <shobhashananency@gmail.com>
2021-02-12 14:24:57 -0500

    Calculate ditance and show in snippet, Address show in toast

--- app/src/main/java/com/nency/maps/MapsActivity.java
+++ app/src/main/java/com/nency/maps/MapsActivity.java
@@ -8,10 +8,14 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,21 +28,26 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private static final String TAG = "MapsActivity";
     public static final int REQUEST_CODE = 1;
 
     private Marker homeMarker;
+    private Location userLocation;
 
     Polygon shape;
     private static final int POLYGON_SIDES = 4;
-    List<LatLng> latLngList = new ArrayList<>();
+    List<Marker> markers = new ArrayList<>();
+
     List<Double> distancesFromMidPointsOfPolygonEdges = new ArrayList<>();
 
 
@@ -88,7 +97,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onMapLongClick(LatLng latLng) {
-                if (latLngList.size() == POLYGON_SIDES){
+                if (markers.size() == POLYGON_SIDES){
                     clearMap();
                 }
                 if(latLng != null) {
@@ -96,22 +105,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                // Show address in Toast on click of markers
+                Geocoder geocoder;
+                List<Address> addresses;
+                LatLng latLng = marker.getPosition();
+                geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+                try {
+                    addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    Address address = addresses.get(0);
+                    Log.i(TAG, "address:" + address.toString());
+                    Toast.makeText(MapsActivity.this, address.getAddressLine(0),Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(userLocation != null){
+                    Location location = new Location(LocationManager.GPS_PROVIDER);
+                    location.setLatitude( latLng.latitude);
+                    location.setLongitude(latLng.longitude);
+                    float distance = userLocation.distanceTo(location);
+                    marker.setSnippet("Distance : " + distance/1000 + "km");
+                }
+                return false;
+            }
+        });
     }
 
+    // Set marker for polygon
     private void setMarker(LatLng latLng) {
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+        Marker marker = mMap.addMarker(options);
+        int size = markers.size();
+        String title = size == 0 ? "A" : size == 1 ? "B" : size == 2 ? "C" : "D";
+        marker.setTitle(title);
+        marker.showInfoWindow();
 
-        mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        // remove marker
         if (shape != null) {
             shape.remove();
             shape = null;
         }
-        adjustPolygonWithRespectTo(latLng);
+        adjustPolygonWithRespectTo(marker);
         PolygonOptions polygonOptions = null;
-        for (int i = 0; i < latLngList.size(); i++) {
-            if (i == 0)
-                polygonOptions = new PolygonOptions().add(latLngList.get(0));
-            else
-                polygonOptions.add(latLngList.get(i));
+        for (int i = 0; i < markers.size(); i++) {
+            if (i == 0) {
+                polygonOptions = new PolygonOptions().add(markers.get(0).getPosition());
+            } else {
+                polygonOptions.add(markers.get(i).getPosition());
+            }
         }
         polygonOptions.strokeColor(Color.RED);
         polygonOptions.strokeWidth(5f);
@@ -120,31 +168,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private void adjustPolygonWithRespectTo(LatLng point) {
+    private void adjustPolygonWithRespectTo(Marker marker) {
         double minDistance = 0;
 
-        if (latLngList.size() > 2) {
+        if (markers.size() > 2) {
             distancesFromMidPointsOfPolygonEdges.clear();
             //midPointsOfPolygonEdges?.removeAll()
 
-            for (int i = 0; i < latLngList.size(); i++) {
+            for (int i = 0; i < markers.size(); i++) {
                 // 1. Find the mid points of the edges of polygon
-                List<LatLng> list = new ArrayList<>();
+                List<Marker> list = new ArrayList<>();
 
-                if (i == (latLngList.size() - 1)) {
-                    list.add(latLngList.get(latLngList.size() - 1));
-                    list.add(latLngList.get(0));
+                if (i == (markers.size() - 1)) {
+                    list.add(markers.get(markers.size() - 1));
+                    list.add(markers.get(0));
                 } else {
-                    list.add((latLngList.get(i)));
-                    list.add((latLngList.get(i + 1)));
+                    list.add((markers.get(i)));
+                    list.add((markers.get(i + 1)));
                 }
 
                 LatLng midPoint = computeCentroid(list);
 
                 // 2. Calculate the nearest coordinate by finding distance between mid point of each edge and the coordinate to be drawn
                 Location startPoint = new Location("");
-                startPoint.setLatitude(point.latitude);
-                startPoint.setLongitude(point.longitude);
+                startPoint.setLatitude(marker.getPosition().latitude);
+                startPoint.setLongitude(marker.getPosition().longitude);
                 Location endPoint = new Location("");
                 endPoint.setLatitude(midPoint.latitude);
                 endPoint.setLongitude(midPoint.longitude);
@@ -166,42 +214,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             int position = minIndex(distancesFromMidPointsOfPolygonEdges);
 
             // 4. move the nearest coordinate at the end by shifting array right
-            int shiftByNumber = (latLngList.size() - position - 1);
+            int shiftByNumber = (markers.size() - position - 1);
 
-            if (shiftByNumber != latLngList.size()) {
-                latLngList = rotate(latLngList, shiftByNumber);
+            if (shiftByNumber != markers.size()) {
+                markers = rotate(markers, shiftByNumber);
             }
         }
 
         // 5. Now add coordinated to be drawn
-        latLngList.add(point);
-
+        markers.add(marker);
     }
 
-    private List<LatLng> rotate(List<LatLng> latLngList, int shiftByNumber) {
-        if (latLngList.size() == 0)
-            return latLngList;
-        LatLng element = null;
+    private List<Marker> rotate(List<Marker> markerList, int shiftByNumber) {
+        if (markerList.size() == 0)
+            return markerList;
+        Marker element = null;
         for (int i = 0; i < shiftByNumber; i++) {
             // remove last element, add it to front of the ArrayList
-            element = latLngList.remove(latLngList.size() - 1);
-            latLngList.add(0, element);
+            element = markerList.remove(markerList.size() - 1);
+            markerList.add(0, element);
         }
-        return latLngList;
+        return markerList;
     }
 
     public static int minIndex(List<Double> list) {
         return list.indexOf(Collections.min(list));
     }
 
-    private LatLng computeCentroid(List<LatLng> points) {
+    private LatLng computeCentroid(List<Marker> points) {
         double latitude = 0;
         double longitude = 0;
         int n = points.size();
 
-        for (LatLng point : points) {
-            latitude += point.latitude;
-            longitude += point.longitude;
+        for (Marker point : points) {
+            latitude += point.getPosition().latitude;
+            longitude += point.getPosition().longitude;
         }
 
         return new LatLng(latitude / n, longitude / n);
@@ -209,10 +256,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     // To remove markers from map
     private void clearMap() {
-//        for (Marker marker : markers)
-//            marker.remove();
+        for (Marker marker : markers)
+            marker.remove();
 
-        latLngList.clear();
+        markers.clear();
         shape.remove();
         shape = null;
     }
@@ -237,13 +284,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     // set custom home marker
     private void setHomeMarker(Location location) {
-        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-        MarkerOptions options = new MarkerOptions().position(userLocation)
+        userLocation = location;
+        LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions options = new MarkerOptions().position(userLatLng)
                 .title("You are here")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
-                .snippet("Your Location");
+                .snippet("User Location");
         homeMarker = mMap.addMarker(options);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15));
     }
 
 
